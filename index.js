472d5b9f189bed4952d4d792670c4e9c786504f2
Shane McDonough <shanemcdman@gmail.com>
2022-05-02 13:01:01 -0700

    Set up canvas

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-function main(){
-    console.log('Hello World!');
+const CELL_SIZE = 64;
+
+function setup(){
+    let size = calcCanvasSize(CELL_SIZE);
+    createCanvas(size.x, size.y);
 }
 
-main();
+function windowResized(){
+    let size = calcCanvasSize(CELL_SIZE);
+    resizeCanvas(size.x, size.y);
+}
+
+function draw(){
+    background(0);
+}
+
+function calcCanvasSize(cellSize){
+    return createVector(
+        windowWidth - windowWidth % cellSize,
+        windowHeight - windowHeight % cellSize,
+    );
+}
+
+function calcBoardSize(cellSize){
+    return createVector(
+        Math.floor(windowWidth / cellSize),
+        Math.floor(windowHeight / cellSize),
+    );
+}
